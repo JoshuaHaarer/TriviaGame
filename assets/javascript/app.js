251501492755d4a6028f3cdd3bd1ac804a322ec7
joshua <joshuahaarer86@gmail.com>
2018-03-06 16:57:33 -0700

    function tally (doesnt work)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,6 @@ function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
 }
-
 function decrement() {
     timer--;
     $("#timer").html("<h2>" + timer + "</h2>");
@@ -17,19 +16,42 @@ function decrement() {
         alert("Time Up!");
     }
 }
-
 function stop() {
     clearInterval(intervalId);
 }
-
 run();
 
-
-
-
 function check() {
-    $("#after-submit").attr("visibility: hidden");
 
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var correct = 0;
+
+
+    if (question1 === "breakfast") {
+        correct++;
+    } if (question2 === "hell") {
+        correct++;
+    } if (question3 === "tammy") {
+        correct++;
+    }
+
+    var messages = ["Awesome", "You can do better", ".....Really....."];
+    var range;
+        if (correct < 1) {
+            range = 2;
+        }if (correct > 0 && correct < 3) {
+            score = 1;
+        }if (correct > 2) {
+            score = 0;
+        }
+
+
+$("#amount-correct").attr(style = "visible").text("You got " + correct + "correct");
+$("#message").html(messages[range]);
 }
+
+check();
 
 
