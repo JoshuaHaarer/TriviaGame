2ae7a750240e0e67f8d5ad7456cc8cef550294f1
joshua <joshuahaarer86@gmail.com>
2018-03-06 08:15:54 -0700

    images and small layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+//div funtions to make
+        //make a countdown time
+        //a question div function
+        //choices of answers for question
+
+//variables
+    //array of objects for question/answerChoices
+    //countdown timer
+    var timer = 30;
+    var intervalId;
+    //images for correct and incorrect answers
+
+
+
