a6a233e8f2cacbcd0aff85e0473612b4e4fb1622
joshua <joshuahaarer86@gmail.com>
2018-03-06 18:28:08 -0700

    messed with background, no dice

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,17 @@
 var timer = 40;
 var intervalId;
 
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var correct = 0;
+
+var messages = ["Awesome", "You can do better", ".....Really....."];
+var range;
+
+
+
+
 
 function run() {
     clearInterval(intervalId);
@@ -21,35 +32,27 @@ function stop() {
 }
 run();
 
+
+
 function check() {
-
-    var question1 = document.quiz.question1.value;
-    var question2 = document.quiz.question2.value;
-    var question3 = document.quiz.question3.value;
-    var correct = 0;
-
-
     if (question1 === "breakfast") {
         correct++;
     } if (question2 === "hell") {
         correct++;
     } if (question3 === "tammy") {
         correct++;
+    }    
+    if (correct < 1) {
+        range = 2;
+    } if (correct > 0 && correct < 3) {
+        score = 1;
+    } if (correct > 2) {
+        score = 0;
     }
 
-    var messages = ["Awesome", "You can do better", ".....Really....."];
-    var range;
-        if (correct < 1) {
-            range = 2;
-        }if (correct > 0 && correct < 3) {
-            score = 1;
-        }if (correct > 2) {
-            score = 0;
-        }
 
-
-$("#amount-correct").attr(style = "visible").text("You got " + correct + "correct");
-$("#message").html(messages[range]);
+    $("#amount-correct").attr(style = "visible").text("You got " + correct + "correct");
+    $("#message").html(messages[range]);
 }
 
 check();
